31fab866f6805493142237de9d4ce267cec3dc95
sylvianikol <silviaadvert@gmail.com>
2021-02-20 11:25:41 +0200

    created post mapping for create problem

--- src/main/java/com/syn/judgev3/web/ProblemController.java
+++ src/main/java/com/syn/judgev3/web/ProblemController.java
@@ -1,15 +1,20 @@
 package com.syn.judgev3.web;
 
 import com.syn.judgev3.model.binding.ProblemCreateBindingModel;
+import com.syn.judgev3.model.service.ProblemServiceModel;
 import com.syn.judgev3.service.ProblemService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/problems")
@@ -37,5 +42,31 @@ public class ProblemController {
         }
 
         return "create-problem";
+    }
+
+    @PostMapping("/create")
+    public String createConfirm(@Valid ProblemCreateBindingModel problemCreateBindingModel,
+                                BindingResult bindingResult,
+                                RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("problemCreateBindingModel", problemCreateBindingModel);
+            redirectAttributes.addFlashAttribute(
+                    "org.springframework.validation.BindingResult.problemCreateBindingModel",
+                    bindingResult);
+
+            return "redirect:create";
+        }
+
+        boolean isCreated = this.problemService.create(
+                this.modelMapper.map(problemCreateBindingModel, ProblemServiceModel.class));
+
+        if (!isCreated) {
+            redirectAttributes.addFlashAttribute("problemCreateBindingModel", problemCreateBindingModel);
+            redirectAttributes.addFlashAttribute("exists", true);
+
+            return "redirect:create";
+        }
+
+        return "redirect:/";
     }
 }
