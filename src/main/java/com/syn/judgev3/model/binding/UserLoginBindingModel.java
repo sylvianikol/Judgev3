e6c00247b36538b1d3f35751592ac88da47a0704
sylvianikol <silviaadvert@gmail.com>
2021-02-20 10:32:13 +0200

    added validation to UserLoginBindingModel

--- src/main/java/com/syn/judgev3/model/binding/UserLoginBindingModel.java
+++ src/main/java/com/syn/judgev3/model/binding/UserLoginBindingModel.java
@@ -1,5 +1,9 @@
 package com.syn.judgev3.model.binding;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class UserLoginBindingModel {
 
     private String username;
@@ -9,6 +13,8 @@ public class UserLoginBindingModel {
     public UserLoginBindingModel() {
     }
 
+    @NotBlank(message = "Username can not be null or empty")
+    @Size(min = 3, max = 20, message = "Username length must be between 3 and 20 characters")
     public String getUsername() {
         return this.username;
     }
@@ -17,6 +23,8 @@ public class UserLoginBindingModel {
         this.username = username;
     }
 
+    @NotBlank(message = "Password can not be null or empty")
+    @Size(min = 3, max = 20, message = "Password length must be between 3 and 20 characters ")
     public String getPassword() {
         return this.password;
     }
