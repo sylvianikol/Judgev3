d2902f624141adfeecfa2c5378cc091b16e23147
sylvianikol <silviaadvert@gmail.com>
2021-02-20 12:05:45 +0200

    added get mapping for submission create

--- src/main/java/com/syn/judgev3/web/ProblemController.java
+++ src/main/java/com/syn/judgev3/web/ProblemController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -68,5 +69,16 @@ public class ProblemController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping("/submit/{id}")
+    public String submit(@PathVariable String id, HttpSession httpSession, Model model) {
+
+        if (httpSession.getAttribute("user") == null) {
+            return "redirect:/users/login";
+        }
+
+//        ProblemServiceModel problemServiceModel = this.problemService.getById(id);
+        return "create-submission";
     }
 }
