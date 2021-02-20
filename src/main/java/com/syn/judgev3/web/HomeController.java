4cbefd183ec9ec4bb6629dbde61dceb4c7bc520f
sylvianikol <silviaadvert@gmail.com>
2021-02-20 10:08:08 +0200

    added templating to html files

---
+++ src/main/java/com/syn/judgev3/web/HomeController.java
@@ -0,0 +1,36 @@
+package com.syn.judgev3.web;
+
+import com.syn.judgev3.service.ProblemService;
+import com.syn.judgev3.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+    private final ProblemService problemService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public HomeController(ProblemService problemService, ModelMapper modelMapper) {
+        this.problemService = problemService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/")
+    public String index(HttpSession httpSession, Model model) {
+        if (httpSession.getAttribute("user") == null) {
+            return "index";
+        }
+
+        //TODO: this.problemService.getAllByUserId(userId);
+        //TODO: model.addAttribute("problems", problems);
+
+        return "home";
+    }
+}
